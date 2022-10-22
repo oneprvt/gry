a55eaf45dbb5bed65e4023f93f9586da906fbc7e
Slava <36908028+oneprvt@users.noreply.github.com>
2022-10-22 05:09:25 +0300

    Add files via upload

---
+++ src/components/Price.vue
@@ -0,0 +1,241 @@
+<template>
+  <section class="py-8 sm:py-10 lg:py-12">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="max-w-2xl text-center sm:text-left">
+        <h2
+          class="text-2xl font-bold text-gray-900 sm:text-5xl sm:leading-tight"
+        >
+          В честь открытия на все тарифы
+          <span class="relative inline-flex">
+            <span
+              class="
+                bg-gradient-to-r
+                from-[#44BCFF]
+                via-[#FF44EC]
+                to-[#FF675E]
+                blur-lg
+                filter
+                opacity-30
+                w-full
+                h-full
+                absolute
+                inset-0
+              "
+            ></span>
+            <span class="relative"> действует скидка </span>
+          </span>
+        </h2>
+      </div>
+      <div class="mt-12">
+        <a
+          href="./"
+          title="Начать сейчас"
+          class="
+            hidden
+            sm:inline-flex
+            items-center
+            justify-center
+            w-full
+            px-8
+            py-3
+            text-base
+            font-bold
+            text-white
+            transition-all
+            duration-200
+            bg-gray-900
+            border-2 border-transparent
+            sm:w-auto
+            rounded-xl
+            font-pj
+            hover:bg-gray-600
+          "
+          role="button"
+        >
+          Начать сейчас
+        </a>
+      </div>
+      <div
+        class="
+          grid grid-cols-1
+          px-6
+          mt-8
+          text-center
+          sm:text-left sm:px-0
+          gap-x-12 gap-y-16
+          lg:mt-16
+          sm:grid-cols-2
+          lg:grid-cols-4
+        "
+      >
+        <a href="./" class="cursor-pointer">
+          <h3 class="text-lg font-bold text-gray-900 font-pj">
+            1 день
+            <span
+              class="
+                bg-gray-900
+                text-gray-100 text-xs
+                font-semibold
+                ml-2
+                px-3
+                py-1
+                rounded-full
+              "
+              >Скидка 30%</span
+            >
+          </h3>
+          <p class="mt-3 text-sm font-normal text-gray-600 font-pj">
+            Доступ к сервису за 29 рублей в день
+          </p>
+          <div class="flex items-end justify-center mt-6 sm:justify-start">
+            <p class="text-5xl font-bold text-gray-900 font-pj">₽29</p>
+            <p class="text-lg font-medium text-gray-400 font-pj">/ в день</p>
+          </div>
+          <ul class="mt-6 space-y-3 text-base font-medium text-gray-900">
+            <li class="flex items-center">Доступ к API</li>
+            <li class="flex items-center">С ограничениями API *</li>
+            <li class="flex items-center">Круглосуточная поддержка</li>
+            <li class="flex items-center">Доступ ко всем интеграциям</li>
+          </ul>
+        </a>
+        <a href="./" class="cursor-pointer">
+          <h3 class="text-lg font-bold text-gray-900 font-pj">
+            30 дней
+            <span
+              class="
+                bg-gray-900
+                text-gray-100 text-xs
+                font-semibold
+                ml-2
+                px-3
+                py-1
+                rounded-full
+              "
+              >Скидка 50%</span
+            >
+          </h3>
+          <p class="mt-3 text-sm font-normal text-gray-600 font-pj">
+            Доступ к сервису за 6 рублей в день
+          </p>
+          <div class="flex items-end justify-center mt-6 sm:justify-start">
+            <p class="text-5xl font-bold text-gray-900 font-pj">₽180</p>
+            <p class="text-lg font-medium text-gray-400 font-pj">/ в месяц</p>
+          </div>
+          <ul class="mt-6 space-y-3 text-base font-medium text-gray-900">
+            <li class="flex items-center">Доступ к API</li>
+            <li class="flex items-center">Без ограничений API</li>
+            <li class="flex items-center">Круглосуточная поддержка</li>
+            <li class="flex items-center">Доступ ко всем интеграциям</li>
+          </ul>
+        </a>
+        <a href="./" class="cursor-pointer">
+          <h3 class="text-lg font-bold text-gray-900 font-pj">
+            90 дней
+            <span
+              class="
+                bg-gray-900
+                text-gray-100 text-xs
+                font-semibold
+                ml-2
+                px-3
+                py-1
+                rounded-full
+              "
+              >Скидка 70%</span
+            >
+          </h3>
+          <p class="mt-3 text-sm font-normal text-gray-600 font-pj">
+            Доступ к сервису за 4 рубля в день
+          </p>
+          <div class="flex items-end justify-center mt-6 sm:justify-start">
+            <p class="text-5xl font-bold text-gray-900 font-pj">₽360</p>
+            <p class="text-lg font-medium text-gray-400 font-pj">/ в квартал</p>
+          </div>
+          <ul class="mt-6 space-y-3 text-base font-medium text-gray-900">
+            <li class="flex items-center">Доступ к API</li>
+            <li class="flex items-center">Без ограничений API</li>
+            <li class="flex items-center">Круглосуточная поддержка</li>
+            <li class="flex items-center">Доступ ко всем интеграциям</li>
+          </ul>
+          <div class="relative inline-flex mt-10 group">
+            <div
+              class="
+                absolute
+                duration-1000
+                transitiona-all
+                opacity-70
+                -inset-px
+                rounded-xl
+                blur-lg
+                filter
+                group-hover:opacity-100
+                group-hover:-inset-1
+                group-hover:duration-200
+              "
+              style="
+                background: linear-gradient(
+                  90deg,
+                  #44ff9a -0.55%,
+                  #44b0ff 22.86%,
+                  #8b44ff 48.36%,
+                  #ff6644 73.33%,
+                  #ebff70 99.34%
+                );
+              "
+            ></div>
+          </div>
+        </a>
+        <a href="./" class="cursor-pointer">
+          <h3 class="text-lg font-bold text-gray-900 font-pj">
+            360 дней
+            <span
+              class="
+                bg-gray-900
+                text-gray-100 text-xs
+                font-semibold
+                ml-2
+                px-3
+                py-1
+                rounded-full
+              "
+              >Скидка 90%</span
+            >
+          </h3>
+          <p class="mt-3 text-sm font-normal text-gray-600 font-pj">
+            Доступ к сервису за 2 рубля в день
+          </p>
+          <div class="flex items-end justify-center mt-6 sm:justify-start">
+            <p class="text-5xl font-bold text-gray-900 font-pj">₽720</p>
+            <p class="text-lg font-medium text-gray-400 font-pj">/ в год</p>
+          </div>
+          <ul class="mt-6 space-y-3 text-base font-medium text-gray-900">
+            <li class="flex items-center">Доступ к API</li>
+            <li class="flex items-center">Без ограничений API</li>
+            <li class="flex items-center">Круглосуточная поддержка</li>
+            <li class="flex items-center">Доступ ко всем интеграциям</li>
+          </ul>
+        </a>
+      </div>
+      <a href="./" class="cursor-pointer"> </a>
+      <a
+        href="./"
+        title="Условия использования"
+        class="
+          inline-flex
+          text-center text-xs
+          font-medium
+          text-gray-900
+          leading-tight
+          font-pj
+          bg-gray-100
+          p-3
+          rounded-2xl
+          mt-12
+          tracking-wide
+        "
+      >
+        Подробная информация ограничений находится в условиях.
+      </a>
+    </div>
+  </section>
+</template>
